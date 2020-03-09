91890a552aad72830f490c512941bf4c5eddec05
Dhruv <drv_muk@outlook.com>
2020-03-09 16:52:28 +0530

    X and y variables created for training

--- CarSalesPrediction/carSalesPrediction.py
+++ CarSalesPrediction/carSalesPrediction.py
@@ -8,3 +8,14 @@ Created on Sun Feb 23 10:53:36 2020
 '''Import Libraries'''
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+'''Import Dataset'''
+car_df = pd.read_csv('Car_Purchasing_Data.csv', encoding = 'ISO-8859-1')
+
+sns.pairplot(car_df)
+
+'''Preparing dataset for DL'''
+X = car_df.drop(['Customer Name','Customer e-mail','Country','Car Purchase Amount'], axis=1)
+y = car_df['Car Purchase Amount']
