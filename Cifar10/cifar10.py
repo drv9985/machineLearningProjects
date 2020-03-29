1f2d2560bb4a1c07dc273a395a62fbda9b52f3a0
Dhruv <drv_muk@outlook.com>
2020-03-29 20:29:36 +0530

    completed visualization phase in CIFAR 10

--- Cifar10/cifar10.py
+++ Cifar10/cifar10.py
@@ -25,3 +25,28 @@ y_test.shape
 i = 1000
 plt.imshow(X_train[i])
 print(y_train[i])
+
+#Visualize some random images
+W_grid = 15
+L_grid = 15
+
+fig, axes = plt.subplots(L_grid, W_grid, figsize = (25, 25))
+axes = axes.ravel()
+
+n_training = len(X_train)
+
+for i in np.arange(0, L_grid * W_grid):
+    index = np.random.randint(0, n_training) # pick a random number
+    axes[i].imshow(X_train[index])
+    axes[i].set_title(y_train[index])#show label of the images
+    axes[i].axis('off') # get rid of x and y axis in image
+    
+plt.subplots_adjust(hspace = 0.4)
+
+'''Data Preparation'''
+  
+    
+    
+    
+    
+    
